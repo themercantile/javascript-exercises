5f4c2528d0dd9e3a20194edb4a4c09e705315cc2
Tim Kemp <mercantile.code@gmail.com>
2023-03-15 22:04:39 +0800

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(repeatPhrase, numRepeats) {
+    let returnString = "";
+    if (numRepeats < 0) {
+        return("ERROR");
+    } else {
+            for (let count = 0; count < numRepeats; count ++) {
+                returnString += repeatPhrase;
+            }
+        return(returnString);
+    }
+    
 };
-
+// repeatString('hey', 3) // returns 'heyheyhey'
 // Do not edit below this line
 module.exports = repeatString;
