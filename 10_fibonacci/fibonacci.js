b6b28f396f80e034dde6ebcaa94a89c93cf926e2
Tim Kemp <mercantile.code@gmail.com>
2023-03-16 20:50:09 +0800

    In progress

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(indexNum) {
+    let myFib = 0;
+    if (indexNum < 0 ) {
+        return ("OOPS");
+    } else if (typeof(indexNum) === 'string') {
+        indexNum = parseInt(indexNum);
+    } 
+    for (let count = 1; count <= indexNum; count ++) {
+        myFib[count] = myFib[count-1] + 
+    }
+    return myFib;
+    
 };
-
+// Given a number, say 4, we start with 0, 1, 1, 2, 3, 5, 8, 13
+// Use typeof to see if strings are asseptable
 // Do not edit below this line
 module.exports = fibonacci;
